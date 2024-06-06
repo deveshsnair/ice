ba473d8ae009d9bf5ec6fbc4dad80194b6a6613b
deveshsnair <deveshsnair@gmail.com>
2024-06-06 13:58:50 +0530

    V0.0.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./public/*.{html,js}'],
   theme: {
+    colors: {
+      pitch: { 
+        '200': "#dfdea9",
+        '100': "#edeed3",
+        '50': "#f8f8ee",
+      },
+    },
     extend: {
       fontFamily: {
         oswald: ['Oswald'],
